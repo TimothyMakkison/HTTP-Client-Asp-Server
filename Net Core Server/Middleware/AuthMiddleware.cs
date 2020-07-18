e2f1ced85af58568b12b5f116bef8181d946b067
Timothy Makkison <timmakkison@gmail.com>
2020-07-18 16:16:22 +0100

    Add AuthMiddleware, checks for apikey and adds claims

---
+++ Net Core Server/Middleware/AuthMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Net_Core_Server.Data;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Net_Core_Server.Middleware
+{
+    public class AuthMiddleware
+    {
+        private readonly RequestDelegate next;
+        public AuthMiddleware(RequestDelegate next) => this.next = next;
+        public async Task InvokeAsync(HttpContext context, UserContext userContext)
+        {
+            var dict = context.Request.Headers;
+            var apiKey = dict["ApiKey"].ToString();
+
+            if (apiKey != "")
+            {
+                UserDataAccess access = new UserDataAccess(userContext);
+                var user = access.TryGet(Guid.Parse(apiKey));
+                if (user != null)
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, user.UserName),
+                        new Claim(ClaimTypes.Role, user.Role),
+                    };
+                    var identityClaim = new ClaimsIdentity(claims, user.ApiKey.ToString());
+
+                    context.User.AddIdentity(identityClaim);
+                }
+            }
+
+            await next(context);
+        }
+    }
+}
