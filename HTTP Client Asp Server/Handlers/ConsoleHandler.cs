04e9739061ca1550689ef6efac9ae867ddd73942
Timothy Makkison <timmakkison@gmail.com>
2020-07-20 16:19:32 +0100

    Edit moved loop logic into console handler

--- HTTP Client Asp Server/Handlers/ConsoleHandler.cs
+++ HTTP Client Asp Server/Handlers/ConsoleHandler.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 public class ConsoleHandler
 {
-    public static List<CommandPair> Commands { get; set; } = new List<CommandPair> 
-    { 
-        new CommandPair("/Help", line=> Commands.ForEach(x => 
+    public static bool Running { get; set; }
+    public void Run()
+    {
+        Running = true;
+        Console.WriteLine("Hello. What would you like to do?");
+
+        while (Running)
         {
-            Console.WriteLine("Listing commands:");
-            Console.WriteLine($"{x.Example}");
-        })){Example="/Help"},
-        new CommandPair("/Clear", line=> Console.Clear()){Example = "/Clear"},
+            var line = Console.ReadLine();
+            Console.Clear();
+            ProcessLine(line);
+            Console.WriteLine("What would you like to do next ?");
+        }
+    }
+    public List<CommandPair> Commands { get; set; } = new List<CommandPair> 
+    { 
+        //new CommandPair("/Help", line=> Commands.ForEach(x => 
+        //{
+        //    Console.WriteLine("Listing commands:");
+        //    Console.WriteLine($"{x.Example}");
+        //})){Example="/Help"},
+        new CommandPair("Exit", line=> Running=false){Example = "/Clear"},
     };
 
     public void ProcessLine(string line)
