5acdd3bb38a87806d69f1b5b28e5f4d63e2a8c05
Timothy Makkison <timmakkison@gmail.com>
2020-07-20 16:15:17 +0100

    Add TalkBack and User sender, contains client side logic

--- HTTP Client Asp Server/Senders/TalkBackSender.cs
+++ HTTP Client Asp Server/Senders/TalkBackSender.cs
@@ -1,16 +1,47 @@
-﻿using System;
+﻿using HTTP_Client_Asp_Server.Handlers;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 
 namespace HTTP_Client_Asp_Server.Senders
 {
-    public class TalkBackSender : ITalkBackSender
+    public class TalkBackSender : BaseSender
     {
-        public TalkBackSender(HttpClient client)
+        public TalkBackSender(HttpClient client) : base(client) { }
+
+        public void HelloWorld(string line)
         {
-            Client = client;
+            var request = new HttpRequestMessage(HttpMethod.Get, "talkback/hello");
+            HttpResponseMessage response = SendAsync(request).Result;
+            var product = GetResponseString(response).Result;
+            Console.WriteLine(product);
         }
-        public HttpClient Client { get; set; }
+        public void Sort(string line)
+        {
+            line = line.Replace("TalkBack Sort", "");
+            line = line.Replace(" ", "");
+
+            var values = Split(line);
+            var valuePairs = values.Select(x => new Pair() { Name = "integer", Value = x });
+
+            var uri = Handlers.UriBuilder.Build("talkback/sort", valuePairs);
+
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            HttpResponseMessage response = SendAsync(request).Result;
+            var product = response.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(product);
+        }
+        private IEnumerable<string> Split(string value)
+        {
+            value = value.Replace("[", "");
+            value = value.Replace("]", "");
+
+            return value.Split(',');
+        }
     }
 }
---
+++ HTTP Client Asp Server/Senders/UserSender.cs
@@ -0,0 +1,112 @@
+﻿using HTTP_Client_Asp_Server.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Xml;
+
+namespace HTTP_Client_Asp_Server.Senders
+{
+    public class UserSender : AuthenticatedSender
+    {
+        public UserSender(HttpClient client, User user) : base(client,user)
+        {
+        }
+
+        public void GetUser(string line)
+        {
+            line = line.Replace("User Get ", "");
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"user/new?username={line}");
+            var response = SendAsync(request).Result;
+            var product = GetResponseString(response).Result;
+            Console.WriteLine(product);
+        }
+        public void NewUser(string line)
+        {
+            var name= line.Replace("User Post ", "");
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "user/new")
+            {
+                Content = ToHttpContent(name)
+            };
+
+            HttpResponseMessage response = SendAsync(request).Result;
+            var product = GetResponseString(response).Result;
+
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                var jObject = JObject.Parse(product);
+                User.Username = jObject["userName"].Value<string>();
+                User.ApiKey = jObject["apiKey"].Value<string>();
+                User.Assigned = true;
+
+                Console.WriteLine("Got API Key");
+            }
+            else
+            {
+                Console.WriteLine(product);
+            }
+        }
+        public void UserSet(string line)
+        {
+            var values = line.Replace("User Set ", "");
+            var parts = values.Split(' ');
+
+            if (parts.Length <= 1)
+            {
+                Console.WriteLine("Invalid input, must contain a valid username and Guid");
+                return;
+            }
+            User.ApiKey = parts.LastOrDefault();
+            User.Username = string.Join(" ", parts.Take(parts.Length - 1));
+            User.Assigned = true;
+        }
+        public void DeleteUser(string line)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, $"user/removeuser?username={User.Username}");
+            var response = SendAuthenticatedAsync(request).Result;
+            if(response == null)
+            {
+                return;
+            }
+
+            var product = GetResponseString(response).Result;
+            Console.WriteLine(product.First().ToString().ToUpper() + product.Substring(1));
+        }
+        public void ChangeRole(string line)
+        {
+            line = line.Replace("User Role ","");
+            var parts = line.Split(' ');
+
+            if (parts.Length <= 1)
+            {
+                Console.WriteLine("Invalid input, must contain a valid username and role");
+                return;
+            }
+
+            var role = parts.LastOrDefault();
+            var username = string.Join(" ", parts.Take(parts.Length - 1));
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"user/changerole")
+            {
+                Content = ToHttpContent(new UserRolePair() { username = username, role = role })
+            };
+
+            var response = SendAuthenticatedAsync(request);
+            if(response == null)
+            {
+                return;
+            }
+            Console.WriteLine(response);
+        }
+    }
+    public class UserRolePair
+    {
+        public string username { get; set; }
+        public string role { get; set; }
+    }
+}
