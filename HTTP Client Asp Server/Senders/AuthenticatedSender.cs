a63a465d9d23e9af438039b43d170a5643d2fcc8
Timothy Makkison <timmakkison@gmail.com>
2020-07-20 16:14:35 +0100

    Add BaseSender contains shared httpcleint and utilities

---
+++ HTTP Client Asp Server/Senders/AuthenticatedSender.cs
@@ -0,0 +1,30 @@
+﻿using HTTP_Client_Asp_Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_Client_Asp_Server.Senders
+{
+    public class AuthenticatedSender : BaseSender
+    {
+
+        public User User { get; set; }
+        public AuthenticatedSender(HttpClient client, User user) : base(client)
+        {
+            User = user;
+        }
+        protected async virtual Task<HttpResponseMessage> SendAuthenticatedAsync(HttpRequestMessage request)
+        {
+            if (!User.Assigned)
+            {
+                Console.WriteLine("Client must get the locally stored username and ApiKey.If they don’t yet exist the console must print “You need to do a User Post or User Set first");
+                return null;
+            }
+            request.Headers.Add("ApiKey", User.ApiKey);
+
+            return await base.SendAsync(request);
+        }
+    }
+}
---
+++ HTTP Client Asp Server/Senders/BaseSender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_Client_Asp_Server.Senders
+{
+    public abstract class BaseSender
+    {
+        public HttpClient Client;
+        public BaseSender(HttpClient client)
+        {
+            this.Client = client;
+        }
+        protected async virtual Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+        {
+            var result = Client.SendAsync(request);
+            Console.WriteLine("...please wait...");
+            return await result;
+        }
+        public async virtual Task<string> GetResponseString(HttpResponseMessage response)
+        {
+            return await response.Content.ReadAsStringAsync();
+        }
+        public virtual HttpContent ToHttpContent(object item)
+        {
+            var stringContent = Newtonsoft.Json.JsonConvert.SerializeObject(item);
+            return new StringContent(stringContent, Encoding.UTF8, "application/json");
+        }
+    }
+}
