7fcba484bbb70ed06952ab6b8f06f136fc2af830
Timothy Makkison <timmakkison@gmail.com>
2020-07-20 16:18:56 +0100

    Add contains logic to build request uris

---
+++ HTTP Client Asp Server/Handlers/UriBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace HTTP_Client_Asp_Server.Handlers
+{
+    public static class UriBuilder
+    {
+        public static string Build(string baseUri, IEnumerable<Pair> valuePairs)
+        {
+            if(valuePairs.Count() == 0)
+            {
+                return baseUri;
+            }
+            baseUri+="?";
+            var pairings = valuePairs.Select(x => $"{x.Name}={x.Value}");
+            var parameters = string.Join('&', pairings);
+            return baseUri += parameters;
+        }
+    }
+
+    public struct Pair
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+    }
+}
