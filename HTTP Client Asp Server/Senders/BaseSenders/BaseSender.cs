0aedb6d605b82182dee99668f0b140e7541da41f
Timothy Makkison <timmakkison@gmail.com>
2020-07-24 23:22:37 +0100

    Clean using CodeMaid

--- HTTP Client Asp Server/Senders/BaseSenders/BaseSender.cs
+++ HTTP Client Asp Server/Senders/BaseSenders/BaseSender.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,20 +8,24 @@ namespace HTTP_Client_Asp_Server.Senders
     public abstract class BaseSender
     {
         public HttpClient Client;
+
         public BaseSender(HttpClient client)
         {
             this.Client = client;
         }
+
         protected async virtual Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
         {
             var result = Client.SendAsync(request);
             Console.WriteLine("...please wait...");
             return await result;
         }
+
         public async virtual Task<string> GetResponseString(HttpResponseMessage response)
         {
             return await response.Content.ReadAsStringAsync();
         }
+
         public virtual HttpContent ToHttpContent(object item)
         {
             var stringContent = Newtonsoft.Json.JsonConvert.SerializeObject(item);
