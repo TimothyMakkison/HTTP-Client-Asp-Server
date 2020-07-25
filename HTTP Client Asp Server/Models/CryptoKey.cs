4f02a8f2717e7075659571c53f9061dad96da408
Timothy Makkison <timmakkison@gmail.com>
2020-07-25 15:49:08 +0100

    Add CrypoKey responsible for asymmetric key storage

---
+++ HTTP Client Asp Server/Models/CryptoKey.cs
@@ -0,0 +1,8 @@
+﻿namespace HTTP_Client_Asp_Server.Models
+{
+    public class CryptoKey
+    {
+        public string Key { get; set; }
+        public bool HasKey => Key != null && Key != string.Empty;
+    }
+}
