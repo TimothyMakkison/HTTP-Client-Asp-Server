c4cdabbbd160a821bbc6054613e4cdc74f42cb37
Timothy Makkison <timmakkison@gmail.com>
2020-08-13 18:10:10 +0100

    Add CommandInvoker, responsible for parsing the readline and invoking the command

---
+++ HTTP Client Asp Server/Handlers/CommandInvoker.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTTP_Client_Asp_Server.Handlers
+{
+    public static class CommandInvoker
+    {
+        public static void Invoke(CommandModel command, string input)
+        {
+            string values = command.Parsing switch
+            {
+                ParseMode.None => input,
+                ParseMode.Parse => input.Replace(command.CommandKey, ""),
+                ParseMode.ParseAndTrim => input.Replace(command.CommandKey, "").Trim(' '),
+                _ => input,
+            };
+
+            command.Operation.Invoke(values);
+        }
+    }
+}
