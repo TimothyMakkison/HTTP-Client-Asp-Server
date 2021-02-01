3ea9f4c50f3e5c75d8941c2d3b7c362258b3bdc6
Timothy Makkison <timmakkison@gmail.com>
2021-02-01 00:27:02 +0000

    Add: AuthFilter

---
+++ Net Core Server/Filters/AuthFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace Net_Core_Server.Filters
+{
+    public class AuthFilter : IAuthorizationFilter
+    {
+        public string Roles { get; set; }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            bool isAuthorized = CheckRolePermission(context.HttpContext.User, Roles);
+
+            if (!isAuthorized)
+                context.Result = new UnauthorizedObjectResult("Unauthorized. Admin access only.");
+        }
+
+        private bool CheckRolePermission(ClaimsPrincipal claims, string condition)
+        {
+            if (Roles == null || Roles == "")
+            {
+                return true;
+            }
+            var role = claims.FindFirstValue(ClaimTypes.Role);
+            return condition.Contains(role);
+        }
+    }
+
+    public class TestFilter : AuthorizeAttribute
+    {
+    }
+}
