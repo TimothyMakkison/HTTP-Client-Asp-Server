a466dbcb100d2d3caae19518171577b1f2869430
Timothy Makkison <timmakkison@gmail.com>
2021-02-06 18:27:12 +0000

    Edit: Made BaseFilter functions static

--- HTTP Client Asp Server/Handlers/CommandAttributeMethodFilter.cs
+++ HTTP Client Asp Server/Handlers/CommandAttributeMethodFilter.cs
@@ -1,24 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using HTTP_Client_Asp_Server.Infrastructure;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
 namespace HTTP_Client_Asp_Server.Handlers
 {
-    public class CommandAttributeMethodFilter : BaseFilter
+    public static class CommandAttributeMethodFilter 
     {
-        public override IEnumerable<T> GetValidMethods<T>(IEnumerable<object> list) where T : class
+        public static IEnumerable<Delegate> BuildValidMethods(this IEnumerable<object> list) 
         {
-            // Get class instances and corresponding collection of valid methods
+            //Filter for classes, then iterate through methods, selecting methods that take the same arguments and return type as T
             IEnumerable<(object instance, IEnumerable<MethodInfo> methods)> validMethods = list.Where(x => x.GetType().IsClass)
                        .Select(instance => (instance, methods: instance.GetType()
                                                                        .GetMethods()
-                                                                       .Where(m => m.GetCustomAttributes(typeof(CommandAttribute), false).Length > 0
-                                                                       && IsMethodCompatibleWithDelegate<T>(m))));
+                                                                       .Where(m => m.HasAttribute<CommandAttribute>())));
 
             // Flatten into class instance, methodinfo pairing.
             var validMethodClassPair = validMethods.SelectMany(a => a.methods, (tuple, method) => (tuple.instance, method));
+
             // Convert into a func of type T.
-            return validMethodClassPair.Select(x => x.method.CreateDelegate(typeof(T), x.instance) as T);
+            return validMethodClassPair.Select(x => x.method.CreateDelegate(x.instance));
         }
     }
 }
--- HTTP Client Asp Server/Infrastructure/ReflectionExtensions.cs
+++ HTTP Client Asp Server/Infrastructure/ReflectionExtensions.cs
@@ -2,12 +2,54 @@
 using System;
 using System.Collections;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace HTTP_Client_Asp_Server.Infrastructure
 {
     public static class ReflectionExtensions
     {
+        public static bool HasAttribute<T>(this MethodInfo methodInfo) where T : Attribute
+        {
+            return methodInfo.GetCustomAttributes(typeof(T), false).Length > 0;
+        }
+
+        /// <summary>
+        /// Compares a method info and Delegate returning true if they match.
+        /// </summary>
+        /// <typeparam name="T">Delegate type.</typeparam>
+        /// <param name="methodInfo">MethodInfo</param>
+        /// <returns>Bool determining if the T and methodInfo match.</returns>
+        public static bool HasReturnType<T>(this MethodInfo methodInfo, Type returnType) where T : class
+        {
+            Type delegateType = typeof(T);
+            MethodInfo delegateSignature = delegateType.GetMethod("Invoke");
+
+            return delegateSignature.ReturnType == methodInfo.ReturnType;
+        }
+
+        public static Delegate CreateDelegate(this MethodInfo methodInfo, object target)
+        {
+            Func<Type[], Type> getType;
+            var isAction = methodInfo.ReturnType.Equals(typeof(void));
+            var types = methodInfo.GetParameters()
+                                  .Select(p => p.ParameterType);
+
+            if (isAction)
+            {
+                getType = Expression.GetActionType;
+            }
+            else
+            {
+                getType = Expression.GetFuncType;
+                types = types.Concat(new[] { methodInfo.ReturnType });
+            }
+
+            return methodInfo.IsStatic
+                ? Delegate.CreateDelegate(getType(types.ToArray()), methodInfo)
+                : Delegate.CreateDelegate(getType(types.ToArray()), target, methodInfo.Name);
+        }
+
         public static TargetType ToTargetType(this Type type)
         {
             return type == typeof(bool)
