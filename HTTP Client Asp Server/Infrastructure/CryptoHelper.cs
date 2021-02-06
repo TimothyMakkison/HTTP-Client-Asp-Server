98783d2d58ed07758a7225255fd6ba6ee45fc200
Timothy Makkison <timmakkison@gmail.com>
2021-02-06 20:16:48 +0000

    Move CryptoHelper into Infrastructure.

--- HTTP Client Asp Server/Infrastructure/CryptoHelper.cs
+++ HTTP Client Asp Server/Infrastructure/CryptoHelper.cs
@@ -3,14 +3,16 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 
-namespace HTTP_Client_Asp_Server.Handlers
+namespace HTTP_Client_Asp_Server.Infrastructure
 {
     public static class CryptoHelper
     {
-        public static byte[] HexToByte(string hex)
+        public static byte[] HexToByte(this string hex)
         {
-            string[] hexCollection = hex.Split("-");
-            return hexCollection.Select(x => Convert.ToByte(x, 16)).ToArray();
+            //Split along dashes and convert hex to bytes.
+            return hex.Split("-")
+                      .Select(x => Convert.ToByte(x, 16))
+                      .ToArray();
         }
 
         public static string AesDecrypt(byte[] cipher, byte[] key, byte[] IV)
