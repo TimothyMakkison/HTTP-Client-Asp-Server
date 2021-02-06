14760d9fa3468371f65435629ef148bd269fca63
Timothy Makkison <timmakkison@gmail.com>
2021-02-06 20:11:59 +0000

    Add HTTP client tests.

---
+++ HTTP ClientTests/ConsoleClass/CommandParserTests.cs
@@ -0,0 +1,22 @@
+﻿using HTTP_Client_Asp_Server.ConsoleClass;
+using HTTP_Client_Asp_Server.Models.CommandModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace HTTP_Client_Asp_Server.ConsoleClass.Tests
+{
+    [TestClass()]
+    public class CommandParserTests
+    {
+        private void Method (IEnumerable<int> collection)
+        {
+
+        }
+        [TestMethod()]
+        public void ParseTest1()
+        {
+            var cd = new CommandData() { CommandKey = "Command" };
+            Assert.Fail();
+        }
+    }
+}
---
+++ HTTP ClientTests/ConsoleClass/TypeConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using CSharpx;
+using HTTP_Client_Asp_Server.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace HTTP_Client_Asp_Server.ConsoleClass.Tests
+{
+    [TestClass()]
+    public class TypeConverterTests
+    {
+        [TestMethod()]
+        public void ChangeTypeScalarTest()
+        {
+            var value = 53;
+            var args = value.ToString().Yield();
+
+            var type = typeof(int);
+
+            var output = TypeConverter.ChangeType(args, type, true, CultureInfo.InvariantCulture, false);
+            output.Match(val => Assert.AreEqual(value, val), () => Assert.Fail());
+        }
+
+        [TestMethod()]
+        public void ChangeTypeIEnumTest()
+        {
+            var values = Enumerable.Range(0, 10);
+            var args = values.Select(x => x.ToString());
+            var type = typeof(IEnumerable<int>);
+
+            var output = TypeConverter.ChangeType(args, type, false, CultureInfo.InvariantCulture, false);
+
+            output.Match(val => Enumerable.SequenceEqual((IEnumerable<int>)val, values), () => Assert.Fail());
+        }
+    }
+}
