742faf6b4539d9a2b8bb35c85501430f5c622f10
Timothy Makkison <timmakkison@gmail.com>
2021-02-10 14:39:27 +0000

    Add ToString override.

--- HTTP Client Asp Server/Models/User.cs
+++ HTTP Client Asp Server/Models/User.cs
@@ -4,5 +4,7 @@
     {
         public string ApiKey { get; set; }
         public string Username { get; set; }
+
+        public override string ToString() => $"User ApiKey: {ApiKey}, Username: {Username}";
     }
 }
