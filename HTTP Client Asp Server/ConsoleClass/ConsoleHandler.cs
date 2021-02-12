3f95e26c43076ff85c5bb11dc8eb50170b38b2da
Timothy Makkison <timmakkison@gmail.com>
2021-02-12 17:35:26 +0000

    Edit add either to print result.

--- HTTP Client Asp Server/ConsoleClass/ConsoleHandler.cs
+++ HTTP Client Asp Server/ConsoleClass/ConsoleHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RailwaySharp;
+using System;
 
 namespace HTTP_Client_Asp_Server.ConsoleClass
 {
@@ -21,7 +22,7 @@ namespace HTTP_Client_Asp_Server.ConsoleClass
                 Console.Clear();
 
                 var output = Handler.Process(line);
-                Console.WriteLine(output);
+                Console.WriteLine(output.Either((o, _) => o, e => string.Join(',', e)));
                 Console.WriteLine("What would you like to do next ?");
             }
         }
