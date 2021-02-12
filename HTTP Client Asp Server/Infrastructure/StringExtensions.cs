f3a0135e4cbeae88640ee5ea814892463458632f
Timothy Makkison <timmakkison@gmail.com>
2021-02-12 15:21:04 +0000

    Add ToArgs function.

--- HTTP Client Asp Server/Infrastructure/StringExtensions.cs
+++ HTTP Client Asp Server/Infrastructure/StringExtensions.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Linq;
 
 namespace HTTP_Client_Asp_Server.Infrastructure
 {
     public static class StringExtensions
     {
+        public static string[] ToArgs(this string input, string commandKey)
+        {
+            return input.Replace(commandKey, "")
+                            .Trim()
+                            .Split()
+                            .Where(arg => arg != default && arg != "")
+                            .ToArray();
+        }
+
         public static bool IsBooleanString(this string value)
         {
             return value.Equals("true", StringComparison.OrdinalIgnoreCase)
