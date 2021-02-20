048e80aaab3a54e3be0ad14bf214a07656e0184f
Timothy Makkison <timmakkison@gmail.com>
2021-02-20 00:31:10 +0000

    Add IBuilder interface.

---
+++ HTTP Client Asp Server/ConsoleClass/IBuilder.cs
@@ -0,0 +1,19 @@
+﻿using HTTP_Client_Asp_Server.Models;
+using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_Client_Asp_Server.ConsoleClass
+{
+    public interface IBuilder
+    {
+        public IBuilder AddCommand(CommandModel command);
+        public IBuilder SetContainer(Container container);
+        public IBuilder Scan();
+        public CommandLineHandler Build();
+
+    }
+}
