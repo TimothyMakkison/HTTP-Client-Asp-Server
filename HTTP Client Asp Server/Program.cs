50639c706a48a9eefcda6169d1351a5e362a633b
Timothy Makkison <timmakkison@gmail.com>
2021-02-21 00:57:16 +0000

    Weird Checkout

--- HTTP Client Asp Server/Program.cs
+++ HTTP Client Asp Server/Program.cs
@@ -7,7 +7,28 @@ namespace HTTP_Client_Asp_Server
         private static void Main()
         {
             string address = @"https://localhost:44391/api/";
+<<<<<<< Updated upstream
             var console = new ConsoleHandler(new CommandLineHandler(address));
+=======
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(address)
+            };
+
+            var container = new Container(_ =>
+            {
+                _.ForSingletonOf<HttpClient>().Use(client);
+                _.ForSingletonOf<UserHandler>();
+                _.ForSingletonOf<CryptoKey>();
+            });
+
+            var handler = new CommandLineBuilder()
+                .SetContainer(container)
+                .AddCommand(new CommandModel("exit", new Action(() => Environment.Exit(0))))
+                .Build();
+
+            var console = new ConsoleHandler(handler);
+>>>>>>> Stashed changes
             console.Run();
         }
     }
