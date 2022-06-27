23cc8b4465d9544f17e40ad5cd2c6a0c658c534e
Timothy Makkison <timmakkison@gmail.com>
2022-06-27 10:49:33 +0100

    Edit switch to minimal apis

--- Net Core Server/Program.cs
+++ Net Core Server/Program.cs
@@ -1,20 +1,44 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Net_Core_Server.Controllers;
+using Net_Core_Server.Data;
 
-namespace Net_Core_Server
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddDbContext<UserContext>(opt =>
+   opt.UseInMemoryDatabase("UserList"));
+
+services.AddScoped<IUserDataAccess, UserDataAccess>();
+services.AddControllers();
+services.AddMvcCore()
+    .AddNewtonsoftJson();
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseMiddleware<Net_Core_Server.Middleware.AuthMiddleware>();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapTalkBackEnpoints();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
